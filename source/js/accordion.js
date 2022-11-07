34bf8dcabc66c130875bb524fa06e87c310001cb
Stanislav B <bazarovstas@gmail.com>
2022-11-07 23:25:23 +0300

    Добавлены состояния для всех элементов, убрана возможность выделения текста в кнопках, меню и активных элементах

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -2,29 +2,10 @@ function toggleAccordion() {
 
   const accordionNavigation = document.querySelector('[data-accordion="1"]');
   const accordionContacts = document.querySelector('[data-accordion="2"]');
-  const buttons = document.querySelectorAll('[data-accordion-button]');
+  // const buttons = document.querySelectorAll('[data-accordion-button]');
 
   accordionNavigation.classList.add('footer-nav--closed');
   accordionContacts.classList.add('contacts--closed');
-
-  function hideAccordions() {
-    accordionNavigation.classList.toggle('footer-nav--closed');
-    accordionContacts.classList.toggle('contacts--closed');
-  }
-
-  function showNavigationAccordion() {
-    accordionNavigation.classList.toggle('footer-nav--closed');
-  }
-
-
-
-
-  // function hideAccordions() {
-  //   const accordions = document.querySelectorAll('[data-accordion]');
-  //   accordions.forEach((item) => {
-  //     item.classList.toggle('')
-  //   })
-  // }
 }
 
 export {toggleAccordion};
