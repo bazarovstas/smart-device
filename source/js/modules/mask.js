1ab9f65363cbf5cc657080f210bcdff60092e477
Stanislav B <bazarovstas@gmail.com>
2022-11-20 15:53:50 +0300

    Исправлены ошибки модального окна

--- source/js/modules/mask.js
+++ source/js/modules/mask.js
@@ -1,13 +1,30 @@
-import IMask from 'imask';
+import IMask, { Masked } from 'imask';
 
 const phoneInputs = document.querySelectorAll('input[type="tel"]');
 
 function initMask() {
+  let phoneField = null;
+
   phoneInputs.forEach(
-      (inputField) => new IMask(inputField, {
-        mask: '+7(000)000-00-00',
-      })
+      (inputField) => {
+
+        phoneField = new IMask(inputField, {
+          mask: '+7(000)000-00-00',
+
+        });
+
+        console.log(phoneField.masked.isComplete);
+
+      }
+
   );
+
+  // if (phoneField.masked.isComplete) {
+  //   console.log('full number');
+  // }
+
 }
+
+
 
 export {initMask};
